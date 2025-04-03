64cf705e251201e933a41ac8f94e714013364ccf
Guillaume Fournier <me@guif.dev>
2025-04-03 17:31:35 +0200

    feat: Add formatting module with Font, ColumnStart, and Clock enums

---
+++ src/am03127/page_content/formatting.rs
@@ -0,0 +1,54 @@
+use std::fmt::Display;
+
+/// Font size
+pub enum Font {
+    /// Normal size (5x7)
+    Normal,
+    /// Bold Size (6x7)
+    Bold,
+    /// Narrow Size (4x7)
+    Narrow,
+    /// Large Size (7x13), for 16 pixel height or more LED display only
+    Large,
+    /// Long Size (5x8), only for height more than 7 pixels
+    Long,
+}
+
+impl Display for Font {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let command = match self {
+            Font::Normal => "<AA>",
+            Font::Bold => "<AB>",
+            Font::Narrow => "<AC>",
+            Font::Large => "<AD>",
+            Font::Long => "<AE>",
+        };
+
+        write!(f, "{command}")
+    }
+}
+pub struct ColumnStart(pub u8);
+
+impl Display for ColumnStart {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("<N{:02X}>", self.0))
+    }
+}
+
+pub enum Clock {
+    /// Date in format [DD/MM/YY]
+    Date,
+    /// Time in format [hh:mm]
+    Time,
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let command = match self {
+            Clock::Date => "<KD>",
+            Clock::Time => "<KT>",
+        };
+
+        write!(f, "{command}")
+    }
+}
