6027cf3f62496ca2b83a9d14048e912a098618a7
Guillaume Fournier <me@guif.dev>
2025-04-28 17:48:54 +0200

    Create own module for routers

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,24 +1,13 @@
 pub mod dto;
+mod router;
 
-use crate::am03127::realtime_clock::DateTime;
-use crate::am03127::schedule::Schedule;
+use crate::WEB_TASK_POOL_SIZE;
 use crate::panel::Panel;
-use crate::{WEB_TASK_POOL_SIZE, am03127::page_content::Page};
-use core::convert::From;
-use dto::{DateTimeDto, PageDto, ScheduleDto};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
 use embassy_time::Duration;
-use picoserve::extract::Json;
-use picoserve::response::StatusCode;
-use picoserve::routing::{get_service, parse_path_segment};
-use picoserve::{
-    AppRouter, AppWithStateBuilder,
-    extract::State,
-    routing::{PathRouter, get},
-};
+use picoserve::{AppRouter, AppWithStateBuilder, routing::PathRouter};
 
 const JSON_DESERIALIZE_BUFFER_SIZE: usize = 128;
-const LOGGER_NAME: &str = "HTTP Server";
 
 #[derive(Clone, Copy)]
 pub struct SharedPanel(pub &'static Mutex<CriticalSectionRawMutex, Panel<'static>>);
@@ -37,180 +26,17 @@ impl picoserve::extract::FromRef<AppState> for SharedPanel {
 #[derive(Debug, Clone, Default)]
 pub struct AppProps;
 
-impl AppProps {
-    pub fn static_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
-        picoserve::Router::new().route(
-            "",
-            get_service(picoserve::response::File::html(include_str!("index.html"))),
-        )
-    }
-
-    pub fn clock_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
-        picoserve::Router::new().route(
-            "",
-            get(
-                |State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
-                    log::info!("{LOGGER_NAME}: Display clock");
-
-                    let mut panel = shared_panel.lock().await;
-                    panel
-                        .display_clock('A')
-                        .await
-                        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
-                },
-            )
-            .post(
-                |State(SharedPanel(shared_panel)): State<SharedPanel>,
-                 Json::<DateTimeDto, JSON_DESERIALIZE_BUFFER_SIZE>(date_time_dto)| async move {
-                    log::info!("{LOGGER_NAME}: Set clock");
-                    let date_time = DateTime::from(date_time_dto);
-
-                    let mut panel = shared_panel.lock().await;
-                    panel
-                        .set_clock(date_time)
-                        .await
-                        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
-                },
-            ),
-        )
-    }
-
-    pub fn page_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
-        picoserve::Router::new().route(
-            ("", parse_path_segment::<char>()),
-            get(
-                |page_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
-                    log::info!("{LOGGER_NAME}: Getting page \"{page_id}\"");
-                    if !is_id_valid(page_id) {
-                        return Err((StatusCode::BAD_REQUEST, "Page id not valid"));
-                    }
-
-                    let mut panel = shared_panel.lock().await;
-                    if let Ok(page) = panel.get_page(page_id).await {
-                        match page {
-                            Some(page) => Ok(Json(page)),
-                            None => Err((StatusCode::NOT_FOUND, "Page not found")),
-                        }
-                    } else {
-                        Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to get Page"))
-                    }
-                },
-            )
-            .post(
-                |page_id: char,
-                 State(SharedPanel(shared_panel)): State<SharedPanel>,
-                 Json::<PageDto, JSON_DESERIALIZE_BUFFER_SIZE>(page_dto)| async move {
-                    log::info!("{LOGGER_NAME}: Setting page \"{page_id}\"");
-                    if !is_id_valid(page_id) {
-                        return Err((StatusCode::BAD_REQUEST, "Page id not valid"));
-                    }
-                    log::debug!("{LOGGER_NAME}: {:?}", page_dto);
-
-                    let page = Page::from_dto_with_id(page_id, page_dto);
-                    let mut panel = shared_panel.lock().await;
-                    panel
-                        .set_page(page_id, page)
-                        .await
-                        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
-                },
-            )
-            .delete(
-                |page_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
-                    if !is_id_valid(page_id) {
-                        return Err((StatusCode::BAD_REQUEST, "Page id not valid"));
-                    }
-                    log::info!("{LOGGER_NAME}: Delete page \"{page_id}\"");
-
-                    let mut panel = shared_panel.lock().await;
-                    panel
-                        .delete_page(page_id)
-                        .await
-                        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to delete page"))
-                },
-            ),
-        )
-    }
-
-    pub fn pages_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
-        picoserve::Router::new().route(
-            "",
-            get(
-                |State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
-                    let mut panel = shared_panel.lock().await;
-                    match panel.get_pages().await {
-                        Ok(pages) => Ok(Json(pages)),
-                        Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to get pages")),
-                    }
-                },
-            ),
-        )
-    }
-    pub fn schedule_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
-        picoserve::Router::new().route(
-            ("", parse_path_segment::<char>()),
-            get(
-                |schedule_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
-                    log::info!("{LOGGER_NAME}: Getting page \"{schedule_id}\"");
-                    if !is_id_valid(schedule_id) {
-                        return Err((StatusCode::BAD_REQUEST, "Schedule id not valid"));
-                    }
-
-                    let mut panel = shared_panel.lock().await;
-                    if let Ok(schedule) = panel.get_schedule(schedule_id).await {
-                        match schedule {
-                            Some(schedule) => Ok(Json(schedule)),
-                            None => Err((StatusCode::NOT_FOUND, "Schedule not found")),
-                        }
-                    }else {
-                        Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to get schedule"))
-                    }
-
-                },
-            ).
-            post(
-                |schedule_id: char,
-                 State(SharedPanel(shared_panel)): State<SharedPanel>,
-                 Json::<ScheduleDto, JSON_DESERIALIZE_BUFFER_SIZE>(schedule)| async move {
-                    log::info!("{LOGGER_NAME}: Setting schedule {schedule_id}");
-                    if !is_id_valid(schedule_id) {
-                        return Err((StatusCode::BAD_REQUEST, "Schedule id not valid"));
-                    }
-                    let schedule = Schedule::from_dto_with_id(schedule, schedule_id);
-
-                    let mut panel = shared_panel.lock().await;
-                    panel.set_schedule(schedule_id, schedule).await.map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
-                },
-            )
-            .delete(
-                |schedule_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
-                    log::info!("{LOGGER_NAME}: Deleting schedule {schedule_id}");
-                    if !is_id_valid(schedule_id) {
-                        return Err((StatusCode::BAD_REQUEST, "Schedule id not valid"));
-                    }
-
-                    let mut panel = shared_panel.lock().await;
-                    panel.delete_schedule(schedule_id).await.map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
-                },
-            ),
-        )
-    }
-}
-
-fn is_id_valid(id: char) -> bool {
-    id > 'A' && id < 'Z'
-}
-
 impl AppWithStateBuilder for AppProps {
     type State = AppState;
     type PathRouter = impl PathRouter<AppState>;
 
     fn build_app(self) -> picoserve::Router<Self::PathRouter, Self::State> {
         picoserve::Router::new()
-            .nest("/", Self::static_router())
-            .nest("/page", Self::page_router())
-            .nest("/pages", Self::pages_router())
-            .nest("/schedule", Self::schedule_router())
-            .nest("/clock", Self::clock_router())
+            .nest("/", router::static_router())
+            .nest("/page", router::page_router())
+            .nest("/pages", router::pages_router())
+            .nest("/schedule", router::schedule_router())
+            .nest("/clock", router::clock_router())
     }
 }
 
---
+++ src/server/router.rs
@@ -0,0 +1,182 @@
+use super::{AppState, JSON_DESERIALIZE_BUFFER_SIZE, SharedPanel, dto};
+use crate::am03127::page_content::Page;
+use crate::am03127::realtime_clock::DateTime;
+use crate::am03127::schedule::Schedule;
+use core::convert::From;
+use dto::{DateTimeDto, PageDto, ScheduleDto};
+use picoserve::extract::Json;
+use picoserve::response::StatusCode;
+use picoserve::routing::{get_service, parse_path_segment};
+use picoserve::{
+    extract::State,
+    routing::{PathRouter, get},
+};
+
+const LOGGER_NAME: &str = "Router";
+
+pub fn static_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
+    picoserve::Router::new().route(
+        "",
+        get_service(picoserve::response::File::html(include_str!("index.html"))),
+    )
+}
+
+pub fn clock_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
+    picoserve::Router::new().route(
+        "",
+        get(
+            |State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
+                log::info!("{LOGGER_NAME}: Display clock");
+
+                let mut panel = shared_panel.lock().await;
+                panel
+                    .display_clock('A')
+                    .await
+                    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
+            },
+        )
+        .post(
+            |State(SharedPanel(shared_panel)): State<SharedPanel>,
+             Json::<DateTimeDto, JSON_DESERIALIZE_BUFFER_SIZE>(date_time_dto)| async move {
+                log::info!("{LOGGER_NAME}: Set clock");
+                let date_time = DateTime::from(date_time_dto);
+
+                let mut panel = shared_panel.lock().await;
+                panel
+                    .set_clock(date_time)
+                    .await
+                    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
+            },
+        ),
+    )
+}
+
+pub fn page_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
+    picoserve::Router::new().route(
+        ("", parse_path_segment::<char>()),
+        get(
+            |page_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
+                log::info!("{LOGGER_NAME}: Getting page \"{page_id}\"");
+                if !is_id_valid(page_id) {
+                    return Err((StatusCode::BAD_REQUEST, "Page id not valid"));
+                }
+
+                let mut panel = shared_panel.lock().await;
+                if let Ok(page) = panel.get_page(page_id).await {
+                    match page {
+                        Some(page) => Ok(Json(page)),
+                        None => Err((StatusCode::NOT_FOUND, "Page not found")),
+                    }
+                } else {
+                    Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to get Page"))
+                }
+            },
+        )
+        .post(
+            |page_id: char,
+             State(SharedPanel(shared_panel)): State<SharedPanel>,
+             Json::<PageDto, JSON_DESERIALIZE_BUFFER_SIZE>(page_dto)| async move {
+                log::info!("{LOGGER_NAME}: Setting page \"{page_id}\"");
+                if !is_id_valid(page_id) {
+                    return Err((StatusCode::BAD_REQUEST, "Page id not valid"));
+                }
+                log::debug!("{LOGGER_NAME}: {:?}", page_dto);
+
+                let page = Page::from_dto_with_id(page_id, page_dto);
+                let mut panel = shared_panel.lock().await;
+                panel
+                    .set_page(page_id, page)
+                    .await
+                    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
+            },
+        )
+        .delete(
+            |page_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
+                if !is_id_valid(page_id) {
+                    return Err((StatusCode::BAD_REQUEST, "Page id not valid"));
+                }
+                log::info!("{LOGGER_NAME}: Delete page \"{page_id}\"");
+
+                let mut panel = shared_panel.lock().await;
+                panel
+                    .delete_page(page_id)
+                    .await
+                    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to delete page"))
+            },
+        ),
+    )
+}
+
+pub fn pages_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
+    picoserve::Router::new().route(
+        "",
+        get(
+            |State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
+                let mut panel = shared_panel.lock().await;
+                match panel.get_pages().await {
+                    Ok(pages) => Ok(Json(pages)),
+                    Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to get pages")),
+                }
+            },
+        ),
+    )
+}
+
+pub fn schedule_router() -> picoserve::Router<impl PathRouter<AppState>, AppState> {
+    picoserve::Router::new().route(
+        ("", parse_path_segment::<char>()),
+        get(
+            |schedule_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
+                log::info!("{LOGGER_NAME}: Getting page \"{schedule_id}\"");
+                if !is_id_valid(schedule_id) {
+                    return Err((StatusCode::BAD_REQUEST, "Schedule id not valid"));
+                }
+
+                let mut panel = shared_panel.lock().await;
+                if let Ok(schedule) = panel.get_schedule(schedule_id).await {
+                    match schedule {
+                        Some(schedule) => Ok(Json(schedule)),
+                        None => Err((StatusCode::NOT_FOUND, "Schedule not found")),
+                    }
+                } else {
+                    Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to get schedule"))
+                }
+            },
+        )
+        .post(
+            |schedule_id: char,
+             State(SharedPanel(shared_panel)): State<SharedPanel>,
+             Json::<ScheduleDto, JSON_DESERIALIZE_BUFFER_SIZE>(schedule)| async move {
+                log::info!("{LOGGER_NAME}: Setting schedule {schedule_id}");
+                if !is_id_valid(schedule_id) {
+                    return Err((StatusCode::BAD_REQUEST, "Schedule id not valid"));
+                }
+                let schedule = Schedule::from_dto_with_id(schedule, schedule_id);
+
+                let mut panel = shared_panel.lock().await;
+                panel
+                    .set_schedule(schedule_id, schedule)
+                    .await
+                    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
+            },
+        )
+        .delete(
+            |schedule_id: char, State(SharedPanel(shared_panel)): State<SharedPanel>| async move {
+                log::info!("{LOGGER_NAME}: Deleting schedule {schedule_id}");
+                if !is_id_valid(schedule_id) {
+                    return Err((StatusCode::BAD_REQUEST, "Schedule id not valid"));
+                }
+
+                let mut panel = shared_panel.lock().await;
+                panel
+                    .delete_schedule(schedule_id)
+                    .await
+                    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, ""))
+            },
+        ),
+    )
+}
+
+fn is_id_valid(id: char) -> bool {
+    id > 'A' && id < 'Z'
+}
