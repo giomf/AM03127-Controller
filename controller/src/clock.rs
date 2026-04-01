3d48a5b8f6361a4576ba9de16a1bbed5e8ca387d
Guillaume Fournier <me@guif.dev>
2026-04-01 22:27:48 +0200

    fix: recreate UDP socket for timing task every iteration

--- controller/src/clock.rs
+++ controller/src/clock.rs
@@ -12,7 +12,7 @@ use time::{OffsetDateTime, macros::offset};
 use crate::{PANEL_INIT_DELAY_SECS, panel::Panel};
 
 const CLOCK_INIT_DELAY_SECS: u64 = PANEL_INIT_DELAY_SECS + 5;
-const CLOCK_UPDATE_INTERVAL_SECS: u64 = 86_000;
+const CLOCK_UPDATE_INTERVAL_SECS: u64 = 86_400; // 24h
 const LOGGER_NAME: &str = "Clock";
 const SNTP_ADDRESS: [u8; 4] = [188, 174, 253, 188];
 const SNTP_PORT: u16 = 123;
@@ -61,23 +61,23 @@ pub async fn timing_task(network_stack: NetworkStack<'static>, panel: &'static P
         "{LOGGER_NAME}: Start clock task. Executing every {CLOCK_UPDATE_INTERVAL_SECS} seconds."
     );
     Timer::after(Duration::from_secs(CLOCK_INIT_DELAY_SECS)).await;
-    let mut rx_meta = [PacketMetadata::EMPTY; 16];
-    let mut rx_buffer = [0; 4096];
-    let mut tx_meta = [PacketMetadata::EMPTY; 16];
-    let mut tx_buffer = [0; 4096];
-    let mut socket = UdpSocket::new(
-        network_stack,
-        &mut rx_meta,
-        &mut rx_buffer,
-        &mut tx_meta,
-        &mut tx_buffer,
-    );
-    socket.bind(SNTP_PORT).unwrap();
     let context = NtpContext::new(DummyTimestampGenerator::default());
     let sntp_address = SocketAddr::from((SNTP_ADDRESS, SNTP_PORT));
 
     loop {
         network_stack.wait_config_up().await;
+        let mut rx_meta = [PacketMetadata::EMPTY; 16];
+        let mut rx_buffer = [0; 4096];
+        let mut tx_meta = [PacketMetadata::EMPTY; 16];
+        let mut tx_buffer = [0; 4096];
+        let mut socket = UdpSocket::new(
+            network_stack,
+            &mut rx_meta,
+            &mut rx_buffer,
+            &mut tx_meta,
+            &mut tx_buffer,
+        );
+        socket.bind(SNTP_PORT).unwrap();
         log::info!("{LOGGER_NAME}: Getting current date");
         match get_time(sntp_address, &socket, context).await {
             Ok(result) => {
